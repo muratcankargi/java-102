cbbb2dfaf44d4738c395ee2578c4adf39980ca63
muratcankargi <muratcankargi@hotmail.com>
2022-11-30 16:19:58 +0300

    Update Main.java

--- GenericList/Main.java
+++ GenericList/Main.java
@@ -2,6 +2,41 @@ package Generic.Task;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        MyList<Integer> liste = new MyList<>();
+        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
+        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+        liste.add(10);
+        liste.add(20);
+        liste.add(30);
+        liste.add(40);
+        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
+        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+        liste.add(50);
+        liste.add(60);
+        liste.add(70);
+        liste.add(80);
+        liste.add(90);
+        liste.add(100);
+        liste.add(110);
+        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
+        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+        */
+        
+        /*
+          MyList<Integer> liste = new MyList<>();
+        liste.add(10);
+        liste.add(20);
+        liste.add(30);
+        System.out.println("2. indisteki değer : " + liste.get(2));
+        liste.remove(2);
+        liste.add(40);
+        liste.set(0, 100);
+        System.out.println("2. indisteki değer : " + liste.get(2));
+        System.out.println(liste.toString());
+        */
+
+
         MyList<Integer> liste = new MyList<>();
         System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
         liste.add(10);
