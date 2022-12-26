a5a491d789e9c9e247c21df874906d7b1e921825
muratcankargi <muratcankargi@hotmail.com>
2022-12-26 15:22:16 +0300

    Create PatikaDev

---
+++ PatikaDev/src/main/java/com/patikaDev/View/LoginGUI.java
@@ -0,0 +1,60 @@
+package com.patikaDev.View;
+
+import com.patikaDev.Helper.Config;
+import com.patikaDev.Helper.Helper;
+import com.patikaDev.Model.Operator;
+import com.patikaDev.Model.User;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginGUI extends JFrame{
+    private JPanel wrapper;
+    private JPanel wtop;
+    private JPanel wbottom;
+    private JTextField txt_user_usarname;
+    private JPasswordField txt_user_pass;
+    private JButton btn_login;
+
+    public LoginGUI(){
+        add(wrapper);
+        setSize(400,400);
+        setLocation(Helper.screenCenterPoint("x",getSize()),Helper.screenCenterPoint("y",getSize()));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle(Config.PROJECT_TITLE);
+        setResizable(false);
+        setVisible(true);
+
+        btn_login.addActionListener(e -> {
+        if(Helper.isFieldEmpty(txt_user_usarname)||Helper.isFieldEmpty(txt_user_pass)){
+            Helper.showMessages("fill");
+        }
+        else {
+            User u= User.getFetch(txt_user_usarname.getText(),txt_user_pass.getText());
+            if(u==null){
+                Helper.showMessages("Kullanıcı Bulunamadı");
+            }
+            else {
+                switch (u.getType()){
+                    case "operator":
+                        OperatorGUI opGUI= new OperatorGUI((Operator) u);
+                        break;
+                    case "educator":
+                        EducatorGUI eduGUI= new EducatorGUI();
+                        break;
+                    case "student":
+                        StudentGUI stGUI= new StudentGUI();
+                        break;
+                }
+                dispose();
+            }
+        }
+        });
+    }
+
+    public static void main(String[] args) {
+        Helper.setLayout();
+        LoginGUI loginGUI= new LoginGUI();
+    }
+}
