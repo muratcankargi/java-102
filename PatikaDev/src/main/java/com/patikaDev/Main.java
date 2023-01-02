b8b13b2cfeca638985e96dc6856f5bab8346953f
muratcankargi <muratcankargi@hotmail.com>
2023-01-02 23:01:58 +0300

    Created Main.java

---
+++ PatikaDev/src/main/java/com/patikaDev/Main.java
@@ -0,0 +1,9 @@
+package com.patikaDev;
+
+import com.patikaDev.View.LoginGUI;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginGUI loginGUI= new LoginGUI();
+    }
+}
