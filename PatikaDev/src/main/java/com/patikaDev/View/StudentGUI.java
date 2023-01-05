caabbd50ebf4c2ecbd1f4290f2ebf8ddbfd01ad3
muratcankargi <muratcankargi@hotmail.com>
2023-01-05 18:50:50 +0300

    Update StudentGUI.java

--- PatikaDev/src/main/java/com/patikaDev/View/StudentGUI.java
+++ PatikaDev/src/main/java/com/patikaDev/View/StudentGUI.java
@@ -14,6 +14,10 @@ public class StudentGUI extends JFrame{
     private JPanel pnl_patikalar;
     private JPanel pnl_icerikler;
     private JPanel pnl_testler;
+    private JTextField txt_patikalar;
+    private JLabel lbl_hosgeldin;
+    private JButton btn_exit;
+    private JButton btn_patika_katil;
     private DefaultTableModel mdl_patika_list;
     private Object[] row_patika_list;
 
@@ -36,7 +40,14 @@ public class StudentGUI extends JFrame{
         tbl_patikalar.getTableHeader().setReorderingAllowed(false); //başlıkları yerinden oynatmayı kaldırıyor
         tbl_patikalar.getColumnModel().getColumn(0).setMaxWidth(100); //başlığın boyutunu ayarlıyor
 
-
+        tbl_patikalar.getSelectionModel().addListSelectionListener(e -> {
+            String selected_patika_name = tbl_patikalar.getSelectedRow() != -1 ? tbl_patikalar.getValueAt(tbl_patikalar.getSelectedRow(), 1).toString() : null;
+            txt_patikalar.setText(selected_patika_name);
+        });
+        btn_exit.addActionListener(e -> {
+            LoginGUI loginGUI= new LoginGUI();
+            dispose();
+        });
     }
 
     private void loadPatikaModel() {
