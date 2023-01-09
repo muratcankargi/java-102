ba051f9fcb1b5769192ec6be98acc31f5ed55cc2
muratcankargi <muratcankargi@hotmail.com>
2023-01-09 14:58:12 +0300

    Update EducatorGUI.java

--- PatikaDev/src/main/java/com/patikaDev/View/EducatorGUI.java
+++ PatikaDev/src/main/java/com/patikaDev/View/EducatorGUI.java
@@ -7,6 +7,8 @@ import com.patikaDev.Model.Course;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
@@ -18,6 +20,14 @@ public class EducatorGUI extends JFrame {
     private JTable tbl_education;
     private JTextField txt_search_education;
     private JTable tbl_contents;
+    private JTextField txt_content_baslik;
+    private JComboBox cmb_egitim_adi;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton içerikEkleButton;
+    private JLabel lbl_content_id;
+    private JButton içerikSilButton;
     private DefaultTableModel mdl_education_list;
     private Object[] row_education_list;
     private DefaultTableModel mdl_contents_list;
@@ -45,12 +55,7 @@ public class EducatorGUI extends JFrame {
         loadEducationModel();
         tbl_education.setModel(mdl_education_list);
         tbl_education.getTableHeader().setReorderingAllowed(false);
-        txt_search_education.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
 
-            }
-        });
         txt_search_education.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -61,12 +66,26 @@ public class EducatorGUI extends JFrame {
 
 
         mdl_contents_list = new DefaultTableModel();
-        Object[] col_contents_list = {"Id", "Başlık", "Açıklama","Link","Patika Adı"};
+        Object[] col_contents_list = {"Id", "Başlık", "Açıklama", "Link", "Patika Adı"};
         mdl_contents_list.setColumnIdentifiers(col_contents_list);
         row_contents_list = new Object[col_contents_list.length];
         loadContensModel();
         tbl_contents.setModel(mdl_contents_list);
         tbl_contents.getTableHeader().setReorderingAllowed(false);
+        txt_content_baslik.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                String query = Contents.searchQuery(txt_content_baslik.getText(), cmb_egitim_adi.getSelectedItem().toString());
+                loadContentsModel(Contents.searchContentsList(query));
+            }
+        });
+        cmb_egitim_adi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String query = Contents.searchQuery(txt_content_baslik.getText(), cmb_egitim_adi.getSelectedItem().toString());
+                loadContentsModel(Contents.searchContentsList(query));
+            }
+        });
     }
 
     private void loadEducationModel() {
@@ -81,7 +100,7 @@ public class EducatorGUI extends JFrame {
                 row_education_list[i++] = obj.getLanguage();
                 mdl_education_list.addRow(row_education_list);
 
-                lbl_welcome.setText("Hoşgeldin,"+obj.getEducator().getName());
+                lbl_welcome.setText("Hoşgeldin," + obj.getEducator().getName());
             }
         }
     }
@@ -98,19 +117,45 @@ public class EducatorGUI extends JFrame {
             mdl_education_list.addRow(row_education_list);
         }
     }
-    private void loadContensModel(){
+
+    private void loadContensModel() {
         DefaultTableModel clearModel = (DefaultTableModel) tbl_contents.getModel();
         clearModel.setRowCount(0);
         int i;
 
         for (Contents obj : Contents.getList()) {
             i = 0;
-                row_contents_list[i++] = obj.getId();
-                row_contents_list[i++] = obj.getTitle();
-                row_contents_list[i++]=obj.getExplanation();
-                row_contents_list[i++]=obj.getLink();
-                row_contents_list[i]=Helper.getPatikaName(obj.getPatika_id());
-                mdl_contents_list.addRow(row_contents_list);
+            row_contents_list[i++] = obj.getId();
+            row_contents_list[i++] = obj.getTitle();
+            row_contents_list[i++] = obj.getExplanation();
+            row_contents_list[i++] = obj.getLink();
+            row_contents_list[i] = Helper.getPatikaName(obj.getPatika_id());
+            mdl_contents_list.addRow(row_contents_list);
+            int counter=0;
+            for(int j=0;j<cmb_egitim_adi.getItemCount();j++) {
+                if (cmb_egitim_adi.getItemAt(j).equals(Helper.getPatikaName(obj.getPatika_id()))) {
+                    counter++;
+                }
             }
+            if(counter==0)
+                cmb_egitim_adi.addItem(Helper.getPatikaName(obj.getPatika_id()));
+        }
+
+
+    }
+
+    private void loadContentsModel(ArrayList<Contents> list) {
+        DefaultTableModel clearModel = (DefaultTableModel) tbl_contents.getModel();
+        clearModel.setRowCount(0);
+        int i;
+        for (Contents obj : list) {
+            i = 0;
+            row_contents_list[i++] = obj.getId();
+            row_contents_list[i++] = obj.getTitle();
+            row_contents_list[i++] = obj.getExplanation();
+            row_contents_list[i++] = obj.getLink();
+            row_contents_list[i] = Helper.getPatikaName(obj.getPatika_id());
+            mdl_contents_list.addRow(row_contents_list);
+        }
     }
 }
