a6692191d7c77a51388d5600465fbd9b08203976
muratcankargi <muratcankargi@hotmail.com>
2023-01-21 18:33:40 +0300

    Update LoginGUI.java

--- PatikaDev/src/main/java/com/patikaDev/View/LoginGUI.java
+++ PatikaDev/src/main/java/com/patikaDev/View/LoginGUI.java
@@ -6,7 +6,8 @@ import com.patikaDev.Model.Operator;
 import com.patikaDev.Model.User;
 
 import javax.swing.*;
-public class LoginGUI extends JFrame{
+
+public class LoginGUI extends JFrame {
     private JPanel wrapper;
     private JPanel wtop;
     private JPanel wbottom;
@@ -15,49 +16,43 @@ public class LoginGUI extends JFrame{
     private JButton btn_login;
     private JButton btn_register;
 
-    public LoginGUI(){
+    public LoginGUI() {
         add(wrapper);
-        setSize(400,400);
-        setLocation(Helper.screenCenterPoint("x",getSize()),Helper.screenCenterPoint("y",getSize()));
+        setSize(400, 400);
+        setLocation(Helper.screenCenterPoint("x", getSize()), Helper.screenCenterPoint("y", getSize()));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle(Config.PROJECT_TITLE);
         setResizable(false);
         setVisible(true);
 
         btn_login.addActionListener(e -> {
-        if(Helper.isFieldEmpty(txt_user_usarname)||Helper.isFieldEmpty(txt_user_pass)){
-            Helper.showMessages("fill");
-        }
-        else {
-            User u= User.getFetch(txt_user_usarname.getText(),txt_user_pass.getText());
-            if(u==null){
-                Helper.showMessages("Kullanıcı Bulunamadı");
-            }
-            else {
-                switch (u.getType()){
-                    case "operator":
-                        OperatorGUI opGUI= new OperatorGUI((Operator) u);
-                        break;
-                    case "educator":
-                        EducatorGUI eduGUI= new EducatorGUI(txt_user_usarname.getText());
-                        break;
-                    case "student":
-                        StudentGUI stGUI= new StudentGUI();
-                        break;
+            if (Helper.isFieldEmpty(txt_user_usarname) || Helper.isFieldEmpty(txt_user_pass)) {
+                Helper.showMessages("fill");
+            } else {
+                User u = User.getFetch(txt_user_usarname.getText(), txt_user_pass.getText());
+                if (u == null) {
+                    Helper.showMessages("Kullanıcı Bulunamadı");
+                } else {
+                    switch (u.getType()) {
+                        case "operator":
+                            OperatorGUI opGUI = new OperatorGUI((Operator) u);
+                            break;
+                        case "educator":
+                            EducatorGUI eduGUI = new EducatorGUI(txt_user_usarname.getText());
+                            break;
+                        case "student":
+                            StudentGUI stGUI = new StudentGUI();
+                            break;
+                    }
+                    dispose();
                 }
-                dispose();
             }
-        }
         });
 
         btn_register.addActionListener(e -> {
-            RegisterGUI registerGUI= new RegisterGUI();
+            RegisterGUI registerGUI = new RegisterGUI();
             dispose();
         });
     }
 
-    public static void main(String[] args) {
-        Helper.setLayout();
-        LoginGUI loginGUI= new LoginGUI();
-    }
 }
